7e8be0fb63e5eb7668b2bcdfea3d6b874fb9e051
harshit9715 <harshit9715@gmail.com>
2022-01-17 23:17:25 +0530

    basic setup

--- bin/oidc-cdk.ts
+++ bin/oidc-cdk.ts
@@ -5,17 +5,12 @@ import { OidcCdkStack } from '../lib/oidc-cdk-stack';
 
 const app = new cdk.App();
 new OidcCdkStack(app, 'OidcCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  stackName: 'gh-oidc-stack',
+  terminationProtection: false,
+  childAccounts: [{
+    accountId: process.env.CDK_DEFAULT_ACCOUNT!,
+    region: 'us-west-1',
+  }],
+  policy_name: '000',
 });
--- lib/oidc-cdk-stack.ts
+++ lib/oidc-cdk-stack.ts
@@ -1,16 +1,43 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnStackSet, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+import * as fs from 'fs'
+import * as path from 'path'
+
+type ac_data = {
+  accountId: string,
+  region: string,
+}
+export interface OidcCdkStackProps extends StackProps {
+  childAccounts: ac_data[],
+  policy_name: string
+}
 export class OidcCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: OidcCdkStackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'OidcCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+    const templateReplicationFile = path.resolve(__dirname, `../roles/${props?.policy_name}.yml`)
+    const templateReplicationData = fs.readFileSync(templateReplicationFile).toString()
+    new CfnStackSet(this, 'StackSet', {
+      stackSetName: `github-oidc-replication`,
+      permissionModel: "SELF_MANAGED",
+      parameters: [
+        {
+          parameterKey: 'GitHubOrg',
+          parameterValue: "harshit9715"
+        },
+        {
+          parameterKey: 'OIDCProviderArn',
+          parameterValue: ""
+        }
+      ],
+      stackInstancesGroup: props?.childAccounts.map(i => ({
+        regions: [i.region],
+        deploymentTargets: {
+          accounts: [i.accountId],
+        },
+      })),
+      templateBody: templateReplicationData
+    })
   }
 }
